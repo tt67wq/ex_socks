cec8a89ee5640ce3cac357fd48464689df26fb42
tt67wq <alex@mys4s.cn>
2020-03-06 08:51:37 +0800

    你每天都很困，因为你被生活所困。

--- apps/server/mix.exs
+++ apps/server/mix.exs
@@ -14,7 +14,7 @@ defmodule Server.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :crypto],
       mod: {Server, []}
     ]
   end
